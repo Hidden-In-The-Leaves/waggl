38b311177ed53737a98fd28e2c8e67f24f794bb1
Juan Pinol <JuannnCodes@gmail.com>
2022-08-12 20:35:31 -0400

    Added basic example of react router usage and refactored to not use reactDOM API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function App () {
   return (
-    <div>This is the app!</div>
+    <div>
+      <h1>This is the app!</h1>
+      {/* to use react router you can wrap elements in a link component
+      and use a route by setting attribute "to" equal
+      to a route that is found in index.js*/}
+      <Link to="/LogIn">
+        <button>This is a Link to Log In "Page"!</button>
+      </Link>
+    </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
+import LogIn from './components/LogIn.jsx';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    //The whole App is wrapped in a browser router component
+  //which holds all of the "routes" which are basically endpoints
+  <BrowserRouter>
+    <Routes>
+      {/* to create a route you use a Route component
+      and set the "endpoint" name with the attribute "path"
+      and point it to a component acting as a page with the element attribute
+       */}
+      <Route path="/" element={<App />} />
+      <Route path="/LogIn" element={<LogIn />} />
+    </Routes>
+  </BrowserRouter>
+)
