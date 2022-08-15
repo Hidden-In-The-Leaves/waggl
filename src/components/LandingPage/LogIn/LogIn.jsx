0af9c17bbd278688fc645c346fa2b0ecc6aae2f1
chenyou <chenyouhuang@gmail.com>
2022-08-15 15:59:24 -0400

    Fix prettier single quote problem

--- src/components/LandingPage/LogIn/LogIn.jsx
+++ src/components/LandingPage/LogIn/LogIn.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import NavBar from "../../NavBar/NavBar";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../../NavBar/NavBar';
 
 export default function LogIn(props) {
   return (
     <div>
-      <NavBar type="welcome" />
+      <NavBar type='welcome' />
       <h1>Log in here!</h1>
-      <Link to="/">
+      <Link to='/'>
         <button>This is a Link to App "Page"!</button>
       </Link>
     </div>
