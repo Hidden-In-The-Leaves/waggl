5d08e8fddc36ee79628874f4ab8f5b15589e9206
xinyuanzheng <xinyuazheng1995@gmail.com>
2022-08-15 12:04:53 -0400

    fix route error and add sql file

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
-const db = require('../database/postgres.js');
-const messageRoutes = require('./routes/messageRoutes.js');
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+const db = require("../database/postgres.js");
+const messageRoutes = require("./routes/messageRoutes.js");
 
-dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 db.connect(function (err) {
+  console.log("here");
   if (err) {
     console.log(err);
   } else {
@@ -16,15 +17,16 @@ db.connect(function (err) {
   }
 });
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, "../dist")));
 
-app.use('/api/message', messageRoutes);
+app.use("/api/message", messageRoutes);
+
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
 //example query:
 //app.get('some endpoint', async (req, res) => {
 // const yourData = await.process.postgresql.query(
-  // 'Your query here;'
+// 'Your query here;'
 // )
 // res.status(200).send(JSON.stringify(yourData))
 // })
