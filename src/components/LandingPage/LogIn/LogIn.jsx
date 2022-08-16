a1a4da70d82565e3d39273445bcc002ac83f15ad
chenyou <chenyouhuang@gmail.com>
2022-08-16 16:30:57 -0400

    Implement Google Login by firebase

--- src/components/LandingPage/LogIn/LogIn.jsx
+++ src/components/LandingPage/LogIn/LogIn.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../../NavBar/NavBar';
+import signInWithGoogle from '../../../Firebase/firebase-config';
 
 export default function LogIn(props) {
   return (
     <div>
       <NavBar type="welcome" />
       <h1>Log in here!</h1>
+      <button onClick={signInWithGoogle}>Signn in with googleUser</button>
       <Link to="/">
         <button>This is a Link to App "Page"!</button>
       </Link>
