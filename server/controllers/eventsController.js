cb14b70800bd92cfb2cb4cf32786333986260e1f
hmaria <mh6417a@gmail.com>
2022-08-15 23:09:51 -0400

    added api for GET events

---
+++ server/controllers/eventsController.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+const db = require('../../database/postgres');
+
+module.exports = {
+  getEvents: ({ query }, res) => {
+    db.query(`
+      SELECT e.id, e.event_name, p.pack_name, e.city, e.state, e.start_time, e.end_time
+      FROM events e
+      INNER JOIN packs p ON e.pack_id = p.id
+      WHERE p.id IN (SELECT up.pack_id FROM users_packs_join up WHERE up.user_id = $1)
+    `, [query.user_id])
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((err) => {
+        console.log('database error - cannot get events', err);
+        res.sendStatus(500);
+      });
+  },
+};
