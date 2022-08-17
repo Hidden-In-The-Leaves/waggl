d21ebebec0edf4abdd753941f05832cb27349a74
Juan Pinol <JuannnCodes@gmail.com>
2022-08-17 11:27:22 -0400

    Styling on event details, map works most of the times.

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Participant from './Participant.jsx';
-import EventMessage from './EventMessage.jsx';
+import Participant from './Participant';
+import EventMessage from './EventMessage';
+import NavBar from '../NavBar/NavBar';
+import AddEvent from './AddEvent';
+import MapContainer from './Map';
 
 export default function EventDetails(props) {
+  const [profilePic, setProfilePic] = useState(
+    'https://media.istockphoto.com/photos/labradoodle-dog-ordering-online-by-internet-for-home-delivery-picture-id1365754761'
+  );
   return (
     <div id="event-details-container">
-      <div style={{ width: '100%', height: '200px', background: 'orange' }}>
-        Nav Bar Here
-        <Link to="/">
-          <button>This is a Link to App "Page"!</button>
-        </Link>
+      <div style={{ width: '100%', height: '200px' }}>
+        <NavBar />
       </div>
+      <AddEvent />
       <div
         style={{
           display: 'inline-block',
@@ -23,32 +27,35 @@ export default function EventDetails(props) {
       >
         <button
           style={{
-            margin: '10px auto',
+            margin: 'auto',
+            marginTop: '20px',
+            height: '50px',
+            width: '250px',
+            fontSize: '25px',
             display: 'block',
-            width: '50%',
-            height: '100px',
+            color: 'white',
+            backgroundColor: '#FF8700',
+            borderRadius: '40px',
+            padding: '3px 10px',
+            borderRadius: '30px',
+            borderColor: '#FF8700',
+            borderStyle: 'solid',
+            hover: {
+              opacity: '60%',
+              cursor: 'pointer',
+            },
           }}
         >
           Edit Event
         </button>
-        <div
-          id="calendar-container"
-          style={{
-            width: '85%',
-            height: '200px',
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            margin: '10px auto',
-          }}
-        >
-          <h2>Location</h2>
+        <h2 style={{ marginLeft: '20px' }}>Location</h2>
+        <div style={{ margin: 'auto' }} id="map">
+          <MapContainer />
         </div>
         <div
           style={{
             width: '85%',
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            margin: '0 auto',
+            margin: '20px auto',
           }}
         >
           <h2>Participants</h2>
@@ -56,18 +63,40 @@ export default function EventDetails(props) {
           <Participant />
         </div>
       </div>
-      <div style={{ display: 'inline-block', width: '70%', height: '1000px' }}>
-        Event Info and post container Here
-        <div style={{ height: '250px', background: 'blue' }}>
-          Event splash picture here?
+      <div style={{ display: 'inline-block', width: '70%', height: '500px' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            height: '250px',
+            position: 'relative',
+            background: 'orange',
+            borderRadius: '10px',
+          }}
+        >
+          <img
+            style={{ height: '250px' }}
+            src={profilePic}
+            alt="profile pic for event"
+          />
         </div>
         <h2>Event Name</h2>
+        <h3>Pack Name</h3>
         <p>Some event details, let's do yoga with puppies or something ikd</p>
         <div>
           <div style={{ display: 'inline-block', margin: '20px' }}>
             profile pic here
           </div>
-          <input type="textfield" style={{ height: '200px', width: '75%' }} />
+          <input
+            type="textfield"
+            style={{
+              height: '200px',
+              width: '75%',
+              border: '1px solid #9F9F9F',
+              borderRadius: '10px',
+              margin: '5px 0 15px 0',
+            }}
+          />
           <input type="file" />
         </div>
         <div
