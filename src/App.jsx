5963311aa7b4472d2309b2888a35fa8ef834e791
Juan Pinol <JuannnCodes@gmail.com>
2022-08-17 12:30:37 -0400

    Pre-meeting merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import NavBar from "./components/NavBar/NavBar";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
 
 export default function App() {
   return (
@@ -9,9 +9,9 @@ export default function App() {
       <h1>This is the app!</h1>
       {/* to use react router you can wrap elements in a link component
       and use a route by setting attribute "to" equal
-      to a route that is found in index.js*/}
+      to a route that is found in index.js */}
       <Link to="/LogIn">
-        <button>This is a Link to the Log In page!</button>
+        <button type="button">This is a Link to the Log In page!</button>
       </Link>
       <Link to="/SignUp">
         <button type="button">This is a Link to the Sign Up page!</button>
