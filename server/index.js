98004cbb2faaef178a61c8b21f7d0295c47fe49a
xinyuanzheng <xinyuazheng1995@gmail.com>
2022-08-18 20:44:57 -0400

    index.js sendfile

--- server/index.js
+++ server/index.js
@@ -44,6 +44,10 @@ app.use('/api/events', eventsRoutes);
 // for test
 app.use('/api/test', testRoutes);
 
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+);
+
 server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
 // example query:
