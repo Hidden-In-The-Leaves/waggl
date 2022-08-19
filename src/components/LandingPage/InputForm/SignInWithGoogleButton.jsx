f1b3ebedd4f2f9ac6ca6eea2211792b59e88e410
chenyou <chenyouhuang@gmail.com>
2022-08-18 20:16:27 -0400

    Add google img to the google buttons

--- src/components/LandingPage/InputForm/SignInWithGoogleButton.jsx
+++ src/components/LandingPage/InputForm/SignInWithGoogleButton.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import { signInWithGoogle } from '../../../Firebase/firebase-config';
-import { GoogleButton } from '../StyledFormComponents';
+// import { signInWithGoogle } from '../../../Firebase/firebase-config';
+import { GoogleButton, CnneterImg, CenterSpan } from '../StyledFormComponents';
 
 export default function SignInWithGoogle({ value, userActionHandler }) {
   return (
     <>
       {/* {GoogleLogo} */}
       <GoogleButton type="button" onClick={userActionHandler}>
-        {/* <img
+        <CnneterImg
           src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
           alt="Google Logo"
           height="20"
           width="20"
-        /> */}
-        {value}
+        />
+        {'\xa0\xa0\xa0\xa0'}
+        <CenterSpan>{value}</CenterSpan>
       </GoogleButton>
     </>
   );
